06e744e410c69435a8621422cc2dd6a95d1808ba
dcheng857 <szejiec@gmail.com>
2023-02-15 11:27:12 -0500

    Handle Page Not Found Error on Netlify

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
-import { store } from "./app/store";
+import { persistor, store } from "./app/store";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+
+import { PersistGate } from "redux-persist/integration/react";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -12,7 +14,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
